ec20d9d4c92fa009ce9b4247abef82786136d6aa
dlopeiro <dribeir4@gmail.com>
2022-12-01 23:16:11 -0300

    useState created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import Head from 'next/head'
 
 import Title from "../src/components/title/title" // we import things like this when the function is being exported as default inside the file
 import Subtitle from "../src/components/subtitle/subtitle" // other than that we have to import them between }{
+import Button from '../src/components/button/button'
 
 function HomePage () {
+    const [click, setClick] = useState(0)
+
+    const handleClick = () => {
+        console.log("handle click")
+        setClick(click + 1)
+    }
+
     return (
         <>
             <Head>
@@ -16,6 +25,8 @@ function HomePage () {
                 <Title>Another title</Title>
                 <Subtitle text="another subitle" />
                 <p>some more stuff written here</p>
+                <Button onClick={ handleClick }>Test button</Button>
+                <p>Times clicked: { click }</p>
             </div>
         </>
     )
