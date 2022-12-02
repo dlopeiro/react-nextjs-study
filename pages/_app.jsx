bfe22c4a761c9fa27a420977dbe6d2e4738556af
dlopeiro <dribeir4@gmail.com>
2022-12-01 22:45:39 -0300

    font imported and title config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+
 import '../src/style.css'
 
 function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>myReact</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,23 @@
+import Head from 'next/head'
+
 import Title from "../src/components/title/title" // we import things like this when the function is being exported as default inside the file
 import Subtitle from "../src/components/subtitle/subtitle" // other than that we have to import them between }{
 
 function HomePage () {
     return (
-        <div>
-            <Title>A title</Title>
-            <Subtitle text="a subtitle" />
-            <p>stuff written here</p>
-            <Title>Another title</Title>
-            <Subtitle text="another subitle" />
-            <p>some more stuff written here</p>
-        </div>
+        <>
+            <Head>
+                <title>other title</title>
+            </Head>
+            <div>
+                <Title>A title</Title>
+                <Subtitle text="a subtitle" />
+                <p>stuff written here</p>
+                <Title>Another title</Title>
+                <Subtitle text="another subitle" />
+                <p>some more stuff written here</p>
+            </div>
+        </>
     )
 }
 
